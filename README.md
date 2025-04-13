c212dd612b0c9b4befc16efafd680a61aa152700
Vladimir Moskalenko <93604107+Vladimir-Moskalenko@users.noreply.github.com>
2025-04-13 19:41:22 +0300

    Update installer and README
    Installer: Now you can uninstall the theme files (Actually just restoring from a backup folder)
    README: Instructions on installing
    README: Instructions on disabling & enabling SIP

--- README.md
+++ README.md
@@ -14,6 +14,41 @@ For other macOS Versions, check out the list below.
 **Warning: read the README files of each of the projects above before attempting to install the .car files! Failing to do so may result in a broken OS!**
 
 
+# Installation
+
+To install the theme:
+1. Download & unzip the project files
+2. Make sure you have [disabled SIP](#disabling-sip)!
+3. Open Terminal. Type the following: (replace `/path/to/files` with the path to the folder. Normally it'll be something like ~/Downloads/AquaLickXV-main. )
+```
+cd /path/to/files
+chmod +x install.sh
+./install.sh
+```
+3. All files should be automatically copied to the locations
+
+# Disabling SIP
+SIP is an additional protection measure introduced by Apple with OS X El Capitan. It prevents users or applications from editing `/System`, `/bin`, `/usr`, `/sbin`. The system appearance configuration files are located in `/System/Library/CoreServices` directory, meaning SIP needs to be disabled in order to change them. To disable SIP:
+1. Enter recovery mode. On intel machines, press and hold `⌘+R` while your computer is starting up. On Apple Silicon machines, shut your Mac down. Then, press and hold the power button untill you see the boot picker. Select "Options", then click Next. 
+(Note! On machines with Apple's T2 Security chip and Apple Silicon, you will be asked to select a volume to recover and enter an administrator's password)
+2. In the menu bar, select "Utilities", and click "Terminal" (alternatively, press ⇧+⌘+T)
+3. Type the following:
+```
+csrutil disable
+csrutil authenticated-root disable
+reboot
+```
+4. Complete the desired changes. In this case, reboot into macOS and [run the installer](#installation).
+5. It is strongly recommended to re-enable SIP after this. To do so, complete steps 1 & 2. Then type:
+```
+csrutil enable
+csrutil authenticated-root enable
+reboot
+```
+
+[Apple Developer | SIP](https://developer.apple.com/documentation/security/disabling-and-enabling-system-integrity-protection)
+
+
 # To-Do:
 
 - [ ] Finish the README
@@ -26,4 +61,5 @@ For other macOS Versions, check out the list below.
 - [ ] Other UI elements
 
 ### misc
-- [ ] .sh installer
+- [x] .sh installer
+- [ ] Enable installation for other macOS versions
--- install.sh
+++ install.sh
@@ -1,56 +1,140 @@
 #!/bin/zsh
 # AquaLickXV installer script
+# v. 1.1
 
-echo Checking macOS version...
 
-if [[ $(sw_vers --productVersion) == *15.5* ]]
-then
-else
-	echo --------------------------------------------------------
-	echo  Support only for macOS 15.5! Attempting to install on 
-	echo  other versions WILL break the OS!
-	echo --------------------------------------------------------
-	echo Aborting...
-	exit
-fi
+install() 
+{
+	echo Checking macOS version...
 
-echo Checking SIP status...
-if [[ $(csrutil status) == *disabled* ]]
-then
-else
-	echo --------------------------------------------------------
-	echo  Please disable System Integrity Protection to continue
-	echo --------------------------------------------------------
-	echo  
-	echo Aborting...
-	exit
-fi
+	if [[ $(sw_vers --productVersion) == *15.5* ]]
+	then
+	else
+		echo "--------------------------------------------------------"
+		echo " Support only for macOS 15.5! Attempting to install on "
+		echo " other versions may break the OS!"
+		echo "--------------------------------------------------------"
+		echo Aborting...
+		exit
+	fi
 
-echo Backing up current .car files...
-mkdir bkup
-cp -r /System/Library/CoreServices/SystemAppearance.bundle/Contents/Resources/ bkup
+	echo Checking SIP status...
+	if [[ $(csrutil status) == *disabled* ]]
+	then
+	else
+		echo "--------------------------------------------------------"
+		echo " Please disable System Integrity Protection to continue"
+		echo "--------------------------------------------------------"
+		echo  
+		echo Aborting...
+		exit
+	fi
 
-mountExit="$(mount)"
-diskID="${mountExit:0:12}"
+	if [[ $(ls) == *bkup* ]]
+	then
+		echo Existing backup detected, skipping .car file backup...
+	else
+		echo Backing up current .car files...
+		mkdir bkup
+		cp -r /System/Library/CoreServices/SystemAppearance.bundle/Contents/Resources/ bkup
+	fi
+	
+	mountExit="$(mount)"
+	diskID="${mountExit:0:12}"
 
-echo Mounting root volume as r/w...
-mkdir ~/lvmnt
-sudo mount -o nobrowse -t apfs $diskID ~/lvmnt
+	echo Mounting root volume as r/w...
+	mkdir ~/lvmnt
+	sudo mount -o nobrowse -t apfs $diskID ~/lvmnt
 
-echo Copying files...
-cp Aqua.car ~/lvmnt/System/Library/CoreServices/SystemAppearance.bundle/Contents/Resources/
-cp DarkAqua.car ~/lvmnt/System/Library/CoreServices/SystemAppearance.bundle/Contents/Resources/
-cp VibrantLight.car ~/lvmnt/System/Library/CoreServices/SystemAppearance.bundle/Contents/Resources/
-cp VibrantDark.car ~/lvmnt/System/Library/CoreServices/SystemAppearance.bundle/Contents/Resources/
+	echo Copying files...
+	cp Aqua.car ~/lvmnt/System/Library/CoreServices/SystemAppearance.bundle/Contents/Resources/
+	cp DarkAqua.car ~/lvmnt/System/Library/CoreServices/SystemAppearance.bundle/Contents/Resources/
+	cp VibrantLight.car ~/lvmnt/System/Library/CoreServices/SystemAppearance.bundle/Contents/Resources/
+	cp VibrantDark.car ~/lvmnt/System/Library/CoreServices/SystemAppearance .bundle/Contents/Resources/
 
-echo Creating bootable APFS snapshot with the applied changes..
-if [[ $(uname -p) == *i386* || $(uname -p) == *x86_64* ]]
-then
-	echo Using command for intel Macs...
-	sudo bless --mount ~/lvmnt --bootefi --create-snapshot
-else
-	echo Using command for Apple Silicon Macs...
-	sudo bless --mount "~/lvmnt/System/Library/CoreServices/" --setBoot --create-snapshot
+	echo Creating bootable APFS snapshot with the applied changes..
+	if [[ $(uname -p) == *i386* || $(uname -p) == *x86_64* ]]
+	then
+		echo Using command for intel Macs...
+		sudo bless --mount ~/lvmnt --bootefi --create-snapshot
+	else
+		echo Using command for Apple Silicon Macs...
+		sudo bless --mount "~/lvmnt/System/Library/CoreServices/" --setBoot --create-snapshot
+	fi
+	echo Install finished! Reboot your computer to apply the changes.
+}
 
-fi
-echo Install finished! Reboot your computer to apply the changes.
+uninstall()
+{
+	echo Attempting to restore from backup...
+
+	if [[ $(ls) == *bkup* ]]
+	then
+	else
+		echo "--------------------------------------------------------"
+		echo " No backup folder detected! To revert changes, please "
+		echo " reinstall macOS."
+		echo "--------------------------------------------------------"
+		echo Aborting...
+		exit
+	fi
+
+	echo Checking SIP status...
+	if [[ $(csrutil status) == *disabled* ]]
+	then
+	else
+		echo "--------------------------------------------------------"
+		echo " Please disable System Integrity Protection to continue"
+		echo "--------------------------------------------------------"
+		echo  
+		echo Aborting...
+		exit
+	fi
+
+	mountExit="$(mount)"
+	diskID="${mountExit:0:12}"
+
+	echo Mounting root volume as r/w...
+	mkdir ~/lvmnt
+	sudo mount -o nobrowse -t apfs $diskID ~/lvmnt
+
+	echo Copying files...
+	cp /bkup/Aqua.car ~/lvmnt/System/Library/CoreServices/SystemAppearance.bundle/Contents/Resources/
+	cp /bkup/DarkAqua.car ~/lvmnt/System/Library/CoreServices/SystemAppearance.bundle/Contents/Resources/
+	cp /bkup/VibrantLight.car ~/lvmnt/System/Library/CoreServices/SystemAppearance.bundle/Contents/Resources/
+	cp /bkup/VibrantDark.car ~/lvmnt/System/Library/CoreServices/SystemAppearance .bundle/Contents/Resources/
+
+	echo Creating bootable APFS snapshot with the applied changes..
+	if [[ $(uname -p) == *i386* || $(uname -p) == *x86_64* ]]
+	then
+		echo Using command for intel Macs...
+		sudo bless --mount ~/lvmnt --bootefi --create-snapshot
+	else
+		echo Using command for Apple Silicon Macs...
+		sudo bless --mount "~/lvmnt/System/Library/CoreServices/" --setBoot --create-snapshot
+	fi
+	echo Install finished! Reboot your computer to apply the changes.
+}
+
+main()
+{
+echo "Options: "
+echo "	1. Install the theme"
+echo "	2. Restore theme from backup"
+echo " "
+choice="1"
+instVar="1"
+uninstVar="2"
+read "?> " choice 
+
+
+case $choice in
+	"0") echo "Removing the backup..."; rm -rf bkup;; # remove the backup
+	"1") install;;
+	"2") uninstall;;
+esac
+}
+
+
+echo "This is the AquaLickXV installer."
+main
